f84092eab920e0581113e01619dcf5d0c7e69303
ArghaPramanik <arghapramanik15@gmail.com>
2023-08-29 20:05:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">Developed by Argha pramanik</div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  key: "e4a17961fba41a74dd587fda4ffcd0b2",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
